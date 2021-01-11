4dbbd9c3528ede41c9fb8bc8ace4014dac4d13ce
19023702 <19023702@myrp.edu.sg>
2021-01-11 12:21:33 +0800

    First Commit

--- js/showWorldPopulation.js
+++ js/showWorldPopulation.js
@@ -1,25 +1,36 @@
 $(document).ready(function () {
     $.ajax({
         type: "GET",
-        url: "getStistics.php",
+        url: "getStatistics.php",
         cache: false,
         dataType: "JSON",
         success: function (response) {
-            
+
             var data = [];
             var labels = [];
             for (i = 0; i < response.length; i++) {
                 data.push(parseInt(response[i].population));
                 labels.push(response[i].country);
-                
-            }
-            
-            var barChart = new Chart($("#barChart"), {
-                type: 'horizontaBar'
-            })
-            var msg = "<tbody><tr><td>" + response.population + "</td><td>" + response.obese + "</td></tr></tbody>";
 
-            $("#obeseTable").html(msg);
+            }
+
+            var barChart = new Chart($("#barChart"), {
+                type: 'horizontalBar',
+                data: {
+                    datasets: [{
+                            data: data,
+                            backgroundColor: "lightblue",
+                            label: 'Population'
+                        }],
+                    labels: labels
+                },
+
+                options: {
+                    responsive: true
+                }
+
+            });
+
         }
     });
 
